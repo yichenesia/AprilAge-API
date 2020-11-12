8841e8b685a7b321b7121384467c79aad3f8e2fe
Angela <aaangelaaa@users.noreply.github.com>
2020-11-12 17:06:42 -0500

    Small change to method name in controller

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -5,15 +5,15 @@ import { connectedToApi, connectedToDatabase } from '../models/email.model.js';
 /*******************************************************************************
 GET /users/:email/userInfo
 *******************************************************************************/
-export const userInfoHealthCheck = async (req, res, next) => {
+export const retrieveUserInfo = async (req, res, next) => {
   try {
     const email = req.params.email;
     const connectedToApiResult = connectedToApi();
     const connectedToDatabaseResult = await connectedToDatabase();
     return res.json({ 
         connectedToAPI: connectedToApiResult , 
-        email: email ,
-        connectedToDB: connectedToDatabaseResult});
+        connectedToDB: connectedToDatabaseResult ,
+        email: email});
   } catch(err) {
     next(err);
   }
--- routes/email.routes.js
+++ routes/email.routes.js
@@ -4,7 +4,7 @@ import * as email from '../controllers/email.controller.js';
 
 const routes = (router) => {
   router.route('/users/:email/userInfo')
-    .get(email.userInfoHealthCheck);
+    .get(email.retrieveUserInfo);
 };
 
 export default routes;
