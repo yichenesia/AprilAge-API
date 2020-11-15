dbc35a5c4d5bdad696fe32a65bffe3c7b6b38543
Natalie <n.ashgriz@mail.utoronto.ca>
2020-11-15 00:36:23 +0000

    wrote route file, wrote structure of controller file and created model file for resultsID

--- controllers/results_id.controller.js
+++
@@ -1,3 +0,0 @@
-'use strict'; 
-
-import {connectedToApi, connectedToDatabase} from '..models/documents.models.js';
---
+++ models/resultsID.model.js
@@ -0,0 +1,7 @@
+'use strict'; 
+
+import config from '../config/config.js'; 
+import db from '../services/db.js';
+import crypto from 'crypto'; 
+import _ from 'lodash'; 
+import {objectToCamelCase} from '.base.model.js'
