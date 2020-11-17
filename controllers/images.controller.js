6aa5b1e7f342a4f171b5b52a6bde9bb6fe4c059b
Rajvi Rana <50223476+rajvirana@users.noreply.github.com>
2020-11-16 23:44:59 -0500

    implemented images post route where it parse multipart form-data and temporarily sends the image back to in json

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -1,14 +1,21 @@
 'use strict';
 
 import { connectedToApi, connectedToDatabase } from '../models/healthCheck.model.js';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const formidable = require("formidable");
+
 
 /*******************************************************************************
 POST /images
 *******************************************************************************/
 export const uploadImageApi = async (req, res, next) => {
     try {
-      const connectedToApiResult = connectedToApi();
-      return res.json({ connected: connectedToApiResult });
+        const form = formidable({multiples: true});
+        form.parse(req, (err, fields, files) => {
+          return res.json({fields, files});
+        });
     } catch(err) {
       next(err);
     }
