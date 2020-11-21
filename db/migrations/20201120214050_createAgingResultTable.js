ab3bd3d41a4fb4d98df5f0bd8aebd5bccd82fcb4
Angela Bi <angela.bi@mail.utoronto.ca>
2020-11-20 21:47:42 -0500

    Added database tables

---
+++ db/migrations/20201120214050_createAgingResultTable.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+    return knex.raw(`CREATE TABLE \`agingResult\` 
+    (  
+        \`id\` INT NOT NULL AUTO_INCREMENT, 
+        \`sequenceType\` VARCHAR(5),
+	    \`sequenceID\` INT,
+	    \`agingDocument\` INT,
+        \`created_at\` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+        \`updated_at\` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (\`id\`),
+        FOREIGN KEY (\`sequenceID\`) REFERENCES agingSequence(\`id\`)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (\`agingDocument\`) REFERENCES agingDocument(\`id\`)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+        CHECK (\`sequenceType\` = "Max72" OR \`sequenceType\` = "Rel40")
+    );
+`); 
+};
+
+exports.down = function(knex) {
+    return knex.raw("DROP TABLE `agingResult`");
+};
--- db/seeds/sqlFiles/ClearData.sql
+++ db/seeds/sqlFiles/ClearData.sql
@@ -5,5 +5,10 @@ SET FOREIGN_KEY_CHECKS=0;
 DELETE FROM entries;
 DELETE FROM jwt_tokens;
 DELETE FROM users;
+DELETE FROM image;
+DELETE FROM agingDocument;
+DELETE FROM agingSequence;
+DELETE FROM agingResult;
+DELETE FROM agedImage;
 
 SET FOREIGN_KEY_CHECKS=1;  
