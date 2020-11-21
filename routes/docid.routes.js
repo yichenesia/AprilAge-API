abe2ac35beec611861c8d0971e4cc71d809bae7b
Yichen (Chloe) Zhao <yichen.zhao@mail.utoronto.ca>
2020-11-20 19:49:41 -0500

    Updated routes to correspond with documentation

--- routes/docid.routes.js
+++ routes/docid.routes.js
@@ -3,15 +3,15 @@
 import * as docid from '../controllers/docid.controller.js';
 
 const routes = (router) => {
-  router.route('/users/:email/documents/:docid')
+  router.route('/users/:email/documents/:docID')
     .get(docid.retrieveAgingDoc);
-  router.route('/users/:email/documents/:docid')
+  router.route('/users/:email/documents/:docID')
     .delete(docid.removeAgingDoc);
-  router.route('/users/:email/documents/:docid/points')
+  router.route('/users/:email/documents/:docID/points')
     .get(docid.points);
-  router.route('/users/:email/documents/:docid/aging')
+  router.route('/users/:email/documents/:docID/aging')
     .post(docid.aging);
-  router.route('/users/:email/documents/:docid/status')
+  router.route('/users/:email/documents/:docID/status')
     .get(docid.status);
 };
 
