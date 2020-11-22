eef31cd44eb88bf7cac8bb003c2a7074af1546b8
Chloe (Yichen) Zhao <yichen.zhao@mail.utoronto.ca>
2020-11-22 17:50:50 -0500

    Updated docid controller GET method to get aging documents from RDS

--- controllers/docid.controller.js
+++ controllers/docid.controller.js
@@ -3,20 +3,47 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { connectedToApi, connectedToDatabase } from '../models/healthCheck.model.js';
 
+import db from '../services/db.js';
+import _ from 'lodash';
+import { objectToCamelCase } from '../models/base.model.js';
+import userModel from '../models/user2.model.js'
+import agingDocModel from '../models/agingDocument.model.js';
+
 /*******************************************************************************
 GET /users/:email/documents/:docID
 *******************************************************************************/
 export const retrieveAgingDoc = async (req, res, next) => {
   try {
-    const connectedToApiResult = connectedToApi();
-    const connectedToDatabaseResult = await connectedToDatabase();
 
-    return res.json({ 
-      connectedToAPI: connectedToApiResult,
-      connectedToDB: connectedToDatabaseResult,
-      agingDoc: '',
-      status: 'Retrieved aging doc.'
-    });
+    const docID = req.params.docID;
+    const email = req.params.email;
+    const agingDoc = await agingDocModel.findById(docID);
+    const user = await userModel.findByEmail(email);
+
+    const sql = 'SELECT agingDocument.* FROM agingDocument WHERE userID = ?';
+      const result = await db.raw(sql, [user.id]).then((sqlResults) => {
+        return(objectToCamelCase(sqlResults[0]));
+      });
+
+    var found = false;
+
+    for (var k in result) {
+      if (result[k].id == docID) {
+        found = true;
+        break
+      }
+    }
+
+    if (agingDoc == undefined) {
+        res.status(404).send("Error 404: Aging Document not found.")
+    }
+    else if (!found){
+        res.status(403).send("Error 403: Document does not belong to user.")
+    }
+    else {
+      return res.json(agingDoc);
+    }
+    
   } catch(err) {
     next(err);
   }
