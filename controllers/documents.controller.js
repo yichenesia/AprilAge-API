d152bace3954ef61fe5d36f1e18adc545fe097b0
Angela Bi <angela.bi@mail.utoronto.ca>
2020-11-22 16:23:03 -0500

    Implemented some parts of post request for documents

--- controllers/documents.controller.js
+++ controllers/documents.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import { connectedToApi, connectedToDatabase } from '../models/healthCheck.model.js';
+import userModel from '../models/user2.model.js'
+import agingDocModel from '../models/agingDocument.model.js'
 
 /*******************************************************************************
 GET /users/:email/documents
@@ -40,24 +42,36 @@ export const retrieveAgingDocs = async (req, res, next) => {
 /*******************************************************************************
 POST /users/:email/documents
 *******************************************************************************/
+//TODO: have checks to make sure the variables passed in are valid
+//TODO: modify return type (should return docID, not userID)
+//BUG: isSample, measurement are not being added correctly in database
 export const createAgingDoc = async (req, res, next) => {
   try {
-    const email = req.params.email;
-    const newDocument = { 
-      'imageID': req.body.imageID,
-      'imageURI': req.body.imageURI,
-      'detectPoints': req.body.detectPoints,
-      'document': req.body.document
-    }
     const connectedToApiResult = connectedToApi();
     const connectedToDatabaseResult = await connectedToDatabase();
-    return res.json({ 
-        connectedToAPI: connectedToApiResult , 
-        connectedToDB: connectedToDatabaseResult,
-        email: email ,
-        newDocument: newDocument,
-        status: 'create an aging document'
-    });
+    const email = req.params.email;
+
+    const user = await userModel.findByEmail(email);
+    
+    const newDocument = {
+      'userId': user.id,
+      'originalImage': req.body.originalImage,
+      'status': req.body.status,
+      'isSample': req.body.isSample,
+      'gender': req.body.gender,
+      'name': req.body.name,
+      'age': req.body.age,
+      'ethnicity': req.body.ethnicity,
+      'height': req.body.height,
+      'weight': req.body.weight,
+      'measurement': req.body.measurement,
+      'bounds': req.body.bounds
+    }
+
+    if (connectedToApiResult && connectedToDatabaseResult) {
+      const _ = await agingDocModel.create(newDocument)
+      return res.json(newDocument);
+    }    
   } catch(err) {
     next(err);
   }
