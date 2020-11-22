4bfd95c61d30883ee80d4efbfa86e971cdaf5527
Angela Bi <angela.bi@mail.utoronto.ca>
2020-11-22 17:06:25 -0500

    Added controller functionality for getting document results and fixed minor bugs.

--- controllers/documents.controller.js
+++ controllers/documents.controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
+import db from '../services/db.js';
+import _ from 'lodash';
+import { objectToCamelCase } from '../models/base.model.js';
+
 import { connectedToApi, connectedToDatabase } from '../models/healthCheck.model.js';
 import userModel from '../models/user2.model.js'
 import agingDocModel from '../models/agingDocument.model.js'
@@ -11,6 +15,7 @@ GET /users/:email/documents
 export const retrieveAgingDocs = async (req, res, next) => {
   try {
     const email = req.params.email;
+    const user = await userModel.findByEmail(email);
     const pageNumber = req.body.pageNumber;
     const pageSize = req.body.pageSize
     const orderBy = req.body.orderBy;
@@ -31,6 +36,12 @@ export const retrieveAgingDocs = async (req, res, next) => {
       if (["status", "age", "ethnicity", "gender", "name"].includes(orderBy)) {
         returnObj.orderBy = orderBy
       }
+
+      const sql = 'SELECT agingDocument.* FROM agingDocument WHERE userID = ?';
+      const result = await db.raw(sql, [user.id]).then((sqlResults) => {
+        return(objectToCamelCase(sqlResults[0]));
+      });
+      returnObj.items = result;
 
       return res.json(returnObj);
     }    
@@ -80,18 +91,40 @@ export const createAgingDoc = async (req, res, next) => {
 /*******************************************************************************
 GET /users/:email/documents/results
 *******************************************************************************/
+//TODO: Figure out regex check and use filter to properly query for results
 export const retrieveAgingResults = async (req, res, next) => {
   try {
     const email = req.params.email;
+    const user = await userModel.findByEmail(email);
+    const pageNumber = req.body.pageNumber;
+    const pageSize = req.body.pageSize
+    const orderBy = req.body.orderBy;
+    const filter = req.body.filter;
 
     const connectedToApiResult = connectedToApi();
     const connectedToDatabaseResult = await connectedToDatabase();
-    return res.json({ 
-        connectedToAPI: connectedToApiResult , 
-        connectedToDB: connectedToDatabaseResult,
-        email: email ,
-        status: 'retrieve all aging results for user'
-    });
+    if (connectedToApiResult && connectedToDatabaseResult) {
+
+      const returnObj = {
+        email: email, 
+        pageSize: pageSize
+      }
+
+      if (parseInt(req.body.pageNumber) >= 1) {
+        returnObj.pageNumber = pageNumber
+      }
+      if (["status", "age", "ethnicity", "gender", "name"].includes(orderBy)) {
+        returnObj.orderBy = orderBy
+      }
+
+      const sql = 'SELECT agingResult.id, status, sequenceType, sequenceID, agingDocument FROM agingDocument, agingResult WHERE agingDocument.id = agingResult.agingDocument AND agingDocument.userID = ?';
+      const result = await db.raw(sql, [user.id]).then((sqlResults) => {
+        return(objectToCamelCase(sqlResults[0]));
+      });
+      returnObj.items = result;
+
+      return res.json(returnObj);
+    }    
   } catch(err) {
     next(err);
   }
