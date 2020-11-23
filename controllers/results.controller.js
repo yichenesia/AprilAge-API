7db3cc484070af12b3bb96a3f0c4c2089e681e02
Naslin <naslinrahman@hotmail.com>
2020-11-22 21:28:06 -0500

    implemented results route

--- controllers/results.controller.js
+++ controllers/results.controller.js
@@ -1,26 +1,20 @@
 'use strict';
 
 import { connectedToApi, connectedToDatabase } from '../models/healthCheck.model.js'; // change model later
+import resultModel from "../models/agingResult.model.js"; 
+import AgingDocument from '../models/agingDocument.model.js';
 
-/*******************************************************************************
-GET /healthCheck/api
-*******************************************************************************/
-export const healthCheckApi = async (req, res, next) => {
+//GET aging results for an aging doc
+export const getResults = async (req, res, next) => {
   try {
     const connectedToApiResult = connectedToApi();
-    return res.json({ connected: connectedToApiResult });
-  } catch(err) {
-    next(err);
-  }
-};
-
-/*******************************************************************************
-GET /healthCheck/database
-*******************************************************************************/
-export const healthCheckDatabase = async (req, res, next) => {
-  try {
     const connectedToDatabaseResult = await connectedToDatabase();
-    return res.json({ connected: connectedToDatabaseResult });
+
+    if (connectedToApiResult && connectedToDatabaseResult) {
+      //const docs = await AgingDocument.findById(req.params.docID);
+      const results = await resultModel.findByDOCId(req.params.docID);
+      return res.json({results});
+    }
   } catch(err) {
     next(err);
   }
