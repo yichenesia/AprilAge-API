4cc10c90dfda4940bddac5cf917c87cdf2e668cd
Angela Bi <angela.bi@mail.utoronto.ca>
2020-12-09 15:53:20 -0500

    Refactored SQL and duplicate code

--- controllers/documents.controller.js
+++ controllers/documents.controller.js
@@ -1,9 +1,5 @@
 'use strict';
 
-import db from '../services/db.js';
-import _ from 'lodash';
-import { objectToCamelCase } from '../models/base.model.js';
-
 import userModel from '../models/user2.model.js'
 import agingDocModel from '../models/agingDocument.model.js'
 import imageModel from '../models/images.model.js'
@@ -20,54 +16,18 @@ export const retrieveAgingDocs = async (req, res, next) => {
     return;
   }
   try {
-    // const pageNumber = req.body.pageNumber; // pagination not supported (11/25/20)
-    // const pageSize = req.body.pageSize // pagination not supported (11/25/20)
-    const orderBy = req.body.orderBy;
-    const filter = req.body.filter;
-    const allowed_fields = ["status", "age", "ethnicity", "gender", "name"];
-    const filter_regex = RegExp('.*[<|=|>].*');
-
-    const returnObj = {
-      email: email
-    }
-
-    var sql = 'SELECT agingDocument.* FROM agingDocument WHERE userID = ?';
-
-    if (filter_regex.test(filter)) {
-      const field = RegExp('^\\w*');
-      const operator = RegExp('[<|=|>]');
-      const value = RegExp('\\w*$');
-      if (allowed_fields.includes(filter.match(field)[0])) {
-        returnObj.filter = filter;
-        sql = sql + ' AND ' + filter.match(field)[0] + filter.match(operator)[0] + "\"" + filter.match(value)[0] + "\"";
-      } else {        
-        res.status(400);
-        res.send('Invalid input for filter field');
-        return;
-      }
-    } else if (filter) {        
-      res.status(400);
-      res.send('Invalid input for filter');
-      return;
-    }
-    if (allowed_fields.includes(orderBy)) {
-      returnObj.orderBy = orderBy
-      sql = sql + ' ORDER BY ' + orderBy
-    } else if (orderBy) {        
-      res.status(400);
-      res.send('Invalid input for order by');
-      return;
-    }
+    const parseFilters = makeFilterObj(email, req.body.filter, req.body.orderBy);
+    var returnObj = parseFilters[0];
+    var filters = parseFilters[1];
     
-    const result = await db.raw(sql, [user.id]).then((sqlResults) => {
-      return(objectToCamelCase(sqlResults[0]));
-    });
+    const result = await agingDocModel.findWithFilter(user.id, filters);
     returnObj.items = result;
     returnObj.totalItems = Object.keys(result).length;
 
     return res.json(returnObj);
     
   } catch(err) {
+    console.log(err);
     next(err);
   }
 };
@@ -87,7 +47,7 @@ export const createAgingDoc = async (req, res, next) => {
     // New Document Request Representation
     const imageId = req.body.imageId;
     const imageUri = req.body.imageUri;
-      // Aging Document Representation
+    // Aging Document Representation
     const agingDoc = req.body.document;
     const status = agingDoc.status;
     const isSample = agingDoc.isSample;
@@ -200,6 +160,7 @@ export const createAgingDoc = async (req, res, next) => {
     return res.json(newDocument);   
 
   } catch(err) {
+    console.log(err);
     next(err);
   }
 };
@@ -208,62 +169,69 @@ export const createAgingDoc = async (req, res, next) => {
 GET /users/:email/document/results
 *******************************************************************************/
 export const retrieveAgingResults = async (req, res, next) => {
+  const email = req.params.email;
+  const user = await userModel.findByEmail(email);
+  if (!user) {
+    res.status(404);
+    res.send('User not found');
+    return;
+  }
   try {
-    const email = req.params.email;
-    const user = await userModel.findByEmail(email);
-    if (!user) {
-      res.status(404);
-      res.send('User not found');
-      return;
-    }
-    // const pageNumber = req.body.pageNumber; // pagination not supported (11/25/20)
-    // const pageSize = req.body.pageSize // pagination not supported (11/25/20)
-    const orderBy = req.body.orderBy;
-    const filter = req.body.filter;
-    const allowed_fields = ["status", "age", "ethnicity", "gender", "name"];
-    const filter_regex = RegExp('.*[<|=|>].*');
+    const parseFilters = makeFilterObj(email, req.body.filter, req.body.orderBy);
+    var returnObj = parseFilters[0];
+    var filters = parseFilters[1];
 
-    const returnObj = {
-      email: email
-    }
-
-    var sql = 'SELECT agingResult.id, status, sequenceType, sequenceID, agingDocument FROM agingDocument, agingResult WHERE agingDocument.id = agingResult.agingDocument AND agingDocument.userID = ?';
-
-    if (filter_regex.test(filter)) {
-      const field = RegExp('^\\w*');
-      const operator = RegExp('[<|=|>]');
-      const value = RegExp('\\w*$');
-      if (allowed_fields.includes(filter.match(field)[0])) {
-        returnObj.filter = filter;
-        sql = sql + ' AND ' + filter.match(field)[0] + filter.match(operator)[0] + "\"" + filter.match(value)[0] + "\"";
-      } else {        
-        res.status(400);
-        res.send('Invalid input for filter field');
-        return;
-      }
-    } else if (filter) {        
-      res.status(400);
-      res.send('Invalid input for filter');
-      return;
-    }
-    if (allowed_fields.includes(orderBy)) {
-      returnObj.orderBy = orderBy
-      sql = sql + ' ORDER BY ' + orderBy
-    } else if (orderBy) {        
-      res.status(400);
-      res.send('Invalid input for order by');
-      return;
-    }
-
-    const result = await db.raw(sql, [user.id]).then((sqlResults) => {
-      return(objectToCamelCase(sqlResults[0]));
-    });
+    const result = await agingDocModel.findAgings(user.id, filters);
     returnObj.items = result;
     returnObj.totalItems = Object.keys(result).length;
 
     return res.json(returnObj);
   
   } catch(err) {
+    console.log(err);
     next(err);
   }
+};
+
+const makeFilterObj = (email, filter, orderBy) => {
+  // pagination not supported (11/25/20)
+  const allowed_fields = ["status", "age", "ethnicity", "gender", "name"];
+  const filter_regex = RegExp('.*[<|=|>].*');
+
+  const returnObj = {
+    email: email
+  }
+  var filters = {};
+
+  if (filter_regex.test(filter)) {
+    const field = RegExp('^\\w*'); // get attribute to filter by (e.g. age, name) 
+    const operator = RegExp('[<|=|>]'); // get comparator
+    const value = RegExp('\\w*$'); // get value to filter for
+    if (allowed_fields.includes(filter.match(field)[0])) {
+      returnObj.filter = filter;
+      filters.filter = {
+        field: filter.match(field)[0],
+        operator: filter.match(operator)[0],
+        value: filter.match(value)[0]
+      };
+    } else {        
+      res.status(400);
+      res.send('Invalid input for filter field');
+      return;
+    }
+  } else if (filter) {        
+    res.status(400);
+    res.send('Invalid input for filter');
+    return;
+  }
+  if (allowed_fields.includes(orderBy)) {
+    returnObj.orderBy = orderBy;
+    filters.orderBy = orderBy;
+  } else if (orderBy) {        
+    res.status(400);
+    res.send('Invalid input for order by');
+    return;
+  }
+
+  return [returnObj, filters];
 };
