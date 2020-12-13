e35901061de77eff46ae95c711d747b2b9ee0edf
Angela Bi <32660989+aaangelaaa@users.noreply.github.com>
2020-12-12 22:39:32 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -10,7 +10,7 @@ export AWS_ACCESS_KEY_ID="AKIA2SRCFREAA3RGGDTC"
 export AWS_SECRET_ACCESS_KEY="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"
 export API_SESSION_SECRET="somesecret"
 export API_JWT_EXPIRY="90m"
-export API_REFRESH_SESSION_SECRET=="somesupersecret"
+export API_REFRESH_SESSION_SECRET="somesupersecret"
 export API_REFRESH_EXPIRY="7d"
 export API_REFRESH_REFRESH_JWT_ON_USE=0
 export API_CORS_ALLOW_ORIGIN="http://localhost:8080, https://localhost:8080, http://localhost:3000, http://localhost:8081, https://localhost:8081, http://localhost:3002"
